f8cfa4b11199c104527e4f5d41a9baf1c0718005
Chris Sefkin <student@epicodus.com>
2015-06-11 14:31:53 -0700

    full form

--- models/product.js
+++ models/product.js
@@ -1,5 +1,12 @@
 Auction.Product = DS.Model.extend({
   description: DS.attr(),
   owner: DS.attr(),
+  bid: DS.attr(),
+  price: DS.attr(),
+  story: DS.attr(),
+  image: DS.attr(),
+  year: DS.attr(),
+
+
   type: DS.belongsTo('type', {async: true})
 });
