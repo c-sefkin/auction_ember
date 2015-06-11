4c0d776099abb015f2fbb43920d6d16c63d03d1f
Chris Sefkin <student@epicodus.com>
2015-06-11 12:04:56 -0700

    all the errors

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ Auction.Router.map(function() {
   this.resource('types', {path: '/'});
   this.resource('type', {path: ':type_id'}, function() {
     this.resource('new-product');
+    this.resource('product');
   });
   this.resource('new-type');
   this.resource('recent-products');
